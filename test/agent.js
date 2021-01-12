5a1318466cf2d4e2520af81039f25a3906514774
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-01-12 02:44:13 +0100

    Failing test -> Skip test

--- test/agent.js
+++ test/agent.js
@@ -767,7 +767,7 @@ test('`maxFreeSessions` set to 0 causes to close the session after running throu
 {
 	// See https://github.com/nodejs/node/issues/36883
 	const [major, minor] = process.versions.node.split('.');
-	const testFn = major === 12 && minor >= 20 ? test.serial.failing : test.serial;
+	const testFn = major === 12 && minor >= 20 ? test.serial.skip : test.serial;
 
 	testFn('respects `.maxFreeSessions` changes', singleRequestWrapper, async (t, server) => {
 		const agent = new Agent({
